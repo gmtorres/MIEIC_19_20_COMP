99f9c5ef3f30bca8904611dbee1df1a149b82971
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2020-04-21 16:36:21 +0100

    merged

--- exemplo.java
+++ exemplo.java
@@ -6,14 +6,7 @@ class exemplo {
 	  int whoseturn;
 	  int movesmade;
 	  int[] pieces;
-<<<<<<< HEAD
-	  exemplo pach;
-=======
-	  
-	  public  exemplo(int t) {
-		  whoseturn = t;
-	  }
->>>>>>> 86a5919e73baa8c27306c8312598b0a8ec00d070
+
 
 	// Initializes a Tic Tac Toe object
 	  
@@ -21,8 +14,6 @@ class exemplo {
 	  }
 	  
 	  public boolean init() {
-<<<<<<< HEAD
-	    pach = new exemplo(1);
 	    this.getRow1(1);
 	    return true;
 	  }
@@ -52,25 +43,7 @@ class exemplo {
 	    return in;
 	  }
 
-=======
-	  	int[] g;
-	  	int i;
-	  	exemplo e = new exemplo(2);
-	    row0 = new int[3];
-	    row1 = new int[3];
-	    row2 = new int[3];
-	    pieces = new int[2];
-	    pieces[0] = 1+3;
-	    pieces[1] = 2;
-	    whoseturn = 0;
-	    movesmade = 0;
-	    g = new int[7];
-	    i = g[7];
-	    e.init();
-	    return true;
-	  }
 
->>>>>>> 86a5919e73baa8c27306c8312598b0a8ec00d070
 
 
 }
