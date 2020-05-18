8f9e344d81f970305efa8c41dc0665b343514ada
Inês Marques <up201605542@g.uporto.pt>
2020-05-18 10:12:22 +0100

    Hsort

---
+++ test/fixtures/libs/java/sort.java
@@ -0,0 +1,13 @@
+package fixtures.libs.java;
+
+import java.lang.*;
+import java.util.*;
+
+public class sort {
+    final static boolean equals(int c, int a) {
+        if(c==a)
+        return true;
+        else
+        return false;
+	}
+}
